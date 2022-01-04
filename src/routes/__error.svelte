f16e2af84902261310d67f40a7265cd12f5dbb50
tomitheninja <suditomi@pm.me>
2022-01-04 19:21:37 +0100

    Create custom error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+  export function load(args: any) {
+    return {
+      props: {
+        title: `${args.status}: ${args.error?.message || 'Unknown'}`,
+        json: JSON.stringify(args, null, 5),
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  export let title: string;
+  export let json: string | void;
+</script>
+
+<section>
+  <h1>{title}</h1>
+
+  <code>
+    {json}
+  </code>
+</section>
+
+<style>
+  section {
+    margin-left: 16rem;
+    margin-left: var(--nav-width);
+  }
+</style>
