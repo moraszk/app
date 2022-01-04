6ab1291c776e8d480a54e518d85546793984c92a
tomitheninja <suditomi@pm.me>
2022-01-04 19:38:35 +0100

    Create custom 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,20 @@
+<section>
+  <h1>404 - A keresett oldal nem található</h1>
+
+  <ul>
+    <li>
+      <a href="/">Vissza a főoldalra</a>
+    </li>
+
+    <li>
+      <a href="https://captiveportal.mora.u-szeged.hu/status">A régi státuszoldal</a>
+    </li>
+  </ul>
+</section>
+
+<style>
+  section {
+    margin-left: 16rem;
+    margin-left: var(--nav-width);
+  }
+</style>
